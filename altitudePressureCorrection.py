5a7dab85689e5cf71d22a2ea25fce30b311fdb05
sapphiremu <92813839+sapphiremu@users.noreply.github.com>
2023-03-31 17:38:21 +0200

    Update altitudePressureCorrection.py

--- altitudePressureCorrection.py
+++ altitudePressureCorrection.py
@@ -8,10 +8,12 @@ from math import exp
 def airPressureDecrement(alt, tempC = 20):
     REF_PRESSURE = 101325 # Reference pressure at sea level
     REF_ALTITUDE = 0
+    # There is no reason to change G, M or R unless this software
+    # is to be used on another planet.
     G = 9.80665 # Metres per second squared
     M = 0.0289644 # Molar mass of air
     R = 8.31432 # Universal gas constant
-    T = 273 + tempC # 20 C in kelvins
-    pressure = REF_PRESSURE * exp(-G * M * (alt - REF_ALTITUDE)/(R * T))
+    tempK = 273 + tempC # tempC in kelvins
+    pressure = REF_PRESSURE * exp(-G * M * (alt - REF_ALTITUDE)/(R * tempK))
     pressureReduction = (REF_PRESSURE - pressure)/100
     return (pressureReduction)
