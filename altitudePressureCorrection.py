4096fca4e134a8801066eeabf8d830c81711ddb8
sapphiremu <92813839+sapphiremu@users.noreply.github.com>
2023-04-01 22:50:54 +0200

    Update altitudePressureCorrection.py

--- altitudePressureCorrection.py
+++ altitudePressureCorrection.py
@@ -7,16 +7,14 @@
 # metres that will need to be added to raw pressure readings 
 # for standard readouts.
 from math import exp
-def airPressureDecrement(altitude, tempC = 20, solarBody = "EARTH"):
+def airPressureDecrement(altitude, tempC = 20):
+    solarBody = "EARTH"
     planet = { 
         "EARTH" : [101325, 9.80665, 0.0289644],
         "MARS": [6518, 3.721, 0.044]
         }
     
-    altitudeReference = 0 # Referemce altitude for P
-    # P = 101325 # Reference pressure at sea level in Pascals
-    # G = 9.80665 # Metres per second squared
-    # M = 0.0289644 # Molar mass of air
+    altitudeReference = 0 # Altitude for the pressureReference
     pressureReference, gravity, molarAirMass = planet[solarBody]
     universalGasConstant = 8.31432 # Universal gas constant
     tempK = 273 + tempC # 20 C in kelvins
